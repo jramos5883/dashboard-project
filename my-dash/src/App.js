989021b22be2d5ee6366f0e8b9393f15b7a40250
John Ramos <jramos5883@gmail.com>
2023-05-11 17:58:58 -0700

    Added protected routes, authentication worflow is now complete.

--- my-dash/src/App.js
+++ my-dash/src/App.js
@@ -5,13 +5,22 @@ import HomePage from "./routes/homepage/homepage.comp";
 import AuthPage from "./routes/authpage/authpage.comp";
 import DashBoard from "./routes/dashboard/dashboard.comp";
 
+import ProtectedRoute from "./components/protectedroute/protectedroute.comp";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route index element={<HomePage />} />
         <Route path="auth" element={<AuthPage />} />
-        <Route path="dashboard" element={<DashBoard />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   );
