bc1ed774b2ce285a570a8b7d40e2adeb45d509be
John Ramos <jramos5883@gmail.com>
2023-05-17 19:16:46 -0700

    Working on / testing google calendar

--- my-dash/src/components/googlecalendar/googlecalendar.comp.jsx
+++ my-dash/src/components/googlecalendar/googlecalendar.comp.jsx
@@ -5,7 +5,7 @@ const GoogleCalendar = () => {
     <div className="google-calendar-container container-fluid">
       <h1>Google Calendar!</h1>
       {/* Need to access OAuth Token for permissions to user calendar data. */}
-      <iframe
+      {/* <iframe
         title="User's Google Calendar"
         src="https://calendar.google.com/calendar/embed?src=jrprodev%40gmail.com&ctz=America%2FLos_Angeles"
         style={{ border: 0 }}
@@ -13,7 +13,7 @@ const GoogleCalendar = () => {
         height="600"
         frameborder="0"
         scrolling="no"
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 };
