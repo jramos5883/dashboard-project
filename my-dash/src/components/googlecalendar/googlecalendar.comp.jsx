40daa20d21775ed384618dfe98106318a6e14d60
John Ramos <jramos5883@gmail.com>
2023-05-21 17:45:29 -0700

    Working on google calendar token passing

--- my-dash/src/components/googlecalendar/googlecalendar.comp.jsx
+++ my-dash/src/components/googlecalendar/googlecalendar.comp.jsx
@@ -4,8 +4,8 @@ const GoogleCalendar = () => {
   return (
     <div className="google-calendar-container container-fluid">
       <h1>Google Calendar!</h1>
-      {/* Need to access OAuth Token for permissions to user calendar data. */}
-      {/* <iframe
+      Need to access OAuth Token for permissions to user calendar data.
+      <iframe
         title="User's Google Calendar"
         src="https://calendar.google.com/calendar/embed?src=jrprodev%40gmail.com&ctz=America%2FLos_Angeles"
         style={{ border: 0 }}
@@ -13,7 +13,7 @@ const GoogleCalendar = () => {
         height="600"
         frameborder="0"
         scrolling="no"
-      ></iframe> */}
+      ></iframe>
     </div>
   );
 };
